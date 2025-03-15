e5cc1ebfa66e1d91270b86fb99b4a2176fec0db7
curiouscapybara14 <156867999+curiouscapybara14@users.noreply.github.com>
2025-03-15 10:31:16 +0800

    Delete SsSnailSite directory

--- SsSnailSite/lightbox.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.lightbox-trigger').forEach(img => {
-    img.onclick = function() {
-        document.getElementById('lightbox').style.display = 'block';
-        document.getElementById('lightbox-img').src = this.src;
-    }
-});
